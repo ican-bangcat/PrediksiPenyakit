1a7091e63ab66b70bce87b7cde5dbe99e777a78e
DANI <dani23ti@mahasiswa.pcr.ac.id>
2025-12-02 10:17:08 +0700

    SplashScreen

---
+++ app/src/main/java/com/example/prediksipenyakit/SplashActivity.kt
@@ -0,0 +1,39 @@
+package com.example.prediksipenyakit
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash)
+
+        // Pengaturan padding untuk layar penuh (Edge-to-Edge) bawaan project baru
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        // --- LOGIKA TIMER 3 DETIK ---
+        // 3000ms = 3 detik
+        Handler(Looper.getMainLooper()).postDelayed({
+
+            // 1. Buat Intent untuk pindah ke MainActivity
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+
+            // 2. Hapus SplashActivity dari daftar Back Stack
+            // (Agar kalau user tekan tombol Back di menu utama, tidak kembali ke Splash)
+            finish()
+
+        }, 3000)
+    }
+}
